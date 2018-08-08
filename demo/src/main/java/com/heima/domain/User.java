1e914e2ccb4c086d9bca6739cf550b7cc308b67a
xujiaoshou <625064096@qq.com>
2018-08-08 20:18:18 +0800

    增加user实体类

---
+++ demo/src/main/java/com/heima/domain/User.java
@@ -0,0 +1,16 @@
+package com.heima.domain;
+
+public class User {
+
+	private String username;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	
+}
